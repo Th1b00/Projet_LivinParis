1200f379307a5bda0ff10263fb46bb7277f71225
Th1b00 <michelt0912@gmail.com>
2025-03-24 19:15:41 +0100

    remise a 0

--- Projet_LivinParis/Program.cs
+++ Projet_LivinParis/Program.cs
@@ -11,17 +11,6 @@ namespace Projet_LivinParis
         static void Main(string[] args)
         {
 
-            Console.WriteLine("salut");
-
-        
-
-            int a = 0;
-            int b = 0;
-            int c = 0;
-            int d = 0;
-            int e = 0;
-            Console.WriteLine("abc");
-
         }
     }
 }
