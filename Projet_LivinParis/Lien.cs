1ed1913997ec981c775ff8e367b9a1976f6a7153
Yanismss <yanis.messas@gmail.com>
2025-03-24 19:56:16 +0100

    ajout de la classe lien

--- Projet_LivinParis/Lien.cs
+++ Projet_LivinParis/Lien.cs
@@ -8,5 +8,27 @@ namespace Projet_LivinParis
 {
     internal class Lien
     {
+        /// <summary>
+        /// Noeud de destination du lien.
+        /// </summary>
+        private Noeud destination;
+
+        /// <summary>
+        /// Définit le noeud de destination du lien.
+        /// </summary>
+        public Noeud Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe Lien.
+        /// </summary>
+        /// <param name="dest">Noeud de destination du lien</param>
+        public Lien(Noeud dest)
+        {
+            this.destination = dest;
+        }
     }
 }
