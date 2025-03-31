b77d2bcfb1b52005aa05ed686bbd64c8059b9293
khelilmehiri <mehiri.khelil@gmail.com>
2025-03-31 09:07:22 +0200

    ajout de la solution sql

--- Projet_LivinParis/Program.cs
+++ Projet_LivinParis/Program.cs
@@ -14,7 +14,6 @@ namespace PROJET_PSI
             
             graphe.ChargerGraphe("metro.xlsx");
 
-            // Affichage de la liste d'adjacence du graphe
             Console.WriteLine("\n\nListe d'Adjacence :\n");
             graphe.AfficherListeAdj();
             Console.WriteLine("\nOrdre du graphe: " + graphe.Noeuds.Count);
@@ -24,22 +23,18 @@ namespace PROJET_PSI
                 taille += n.Liens.Count;
             Console.WriteLine("Taille du graphe: " + Convert.ToString((taille / 2) - 1));
 
-            // Demande à l'utilisateur de choisir les stations pour calculer le plus court chemin
             Console.Write("\nEntre quels sommets souhaitez-vous calculer le plus court chemin ?\nSommet a : ");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.Write("Sommet b : ");
             int b = Convert.ToInt32(Console.ReadLine());
 
-            // Calcul du plus court chemin
             Console.WriteLine($"\nDistance entre {a} et {b} :");
 
-            // Appel de la méthode Dijkstra existante qui affiche déjà l'itinéraire
             double dijkstraResult = graphe.Dijkstra(a, b);
             Console.WriteLine("Dijkstra       : " + dijkstraResult + " minutes");
             Console.WriteLine("Bellman-Ford   : " + graphe.BellmanFord(a, b) + " minutes");
-            Console.WriteLine("Floyd-Warshall: " + graphe.FloydWarshall(a, b) + " minutes");
+            Console.WriteLine("Floyd-Warshall : " + graphe.FloydWarshall(a, b) + " minutes");
 
-            // Génération des graphes
             Console.WriteLine("\nGénération du dessin du graphe complet...");
             graphe.DessinerGraphe("graphe_metro_complet.png");
             Console.WriteLine("Le graphe complet a été enregistré sous 'graphe_metro_complet.png'.");
@@ -47,8 +42,6 @@ namespace PROJET_PSI
             Console.WriteLine("\nGénération du graphe du chemin...");
             Console.WriteLine($"\nLe graphe du chemin de {a} à {b} a été généré sous 'chemin_{a}_vers_{b}.png'");
 
-
-            // Fin de l'exécution
             Console.WriteLine("\nAppuyez sur une touche pour quitter.");
             Console.ReadKey();
 
---
+++ Projet_LivinParis/Solution_MySQL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Solution_MySQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString = "SERVER=localhost;DATABASE=livinParis;UID=root;PASSWORD=;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            Console.WriteLine("Création de l'utilisateur ");
+            Console.WriteLine("Nom d'utilisateur : ");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Mot de passe : ");
+            string mdp = Console.ReadLine();
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connexion réussie !\n");
+
+                List<string> requetes = new List<string>
+                {
+                    "SELECT * FROM Cuisinier WHERE Metro_proche = 'Trocadéro';",
+                    "CREATE USER '"+nom+"'@'localhost' identified by '"+mdp+"';",
+                    "GRANT ALL ON livinParis.* to '"+nom+"'@'localhost';"
+
+                };
+
+                foreach (var requete in requetes)
+                {
+                    Console.WriteLine($"\nExécution de la requête :\n{requete}\n");
+
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = requete;
+
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string currentRowAsString = "";
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            string valueAsString = reader.GetValue(i).ToString();
+                            currentRowAsString += valueAsString + ", ";
+                        }
+                        Console.WriteLine(currentRowAsString);
+                    }
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("\nConnexion fermée.");
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
