4a7636cfa7d98d162dc73dfcb28b18bd7cf6e6cc
Yanismss <yanis.messas@gmail.com>
2025-04-04 09:46:27 +0200

    Update Noeud.cs

--- Projet_LivinParis/Noeud.cs
+++ Projet_LivinParis/Noeud.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 namespace PROJET_PSI
 {
     /// <summary>
-    /// Représente un nœud dans un graphe, pouvant être utilisé pour modéliser une station de métro.
+    /// Représente un nœud dans un graphe utilisé pour modéliser une station de métro.
     /// </summary>
-    /// <typeparam name="T"> Liens sortants du nœud.</typeparam>
+    /// <typeparam name="T"> Liens sortants du noeud.</typeparam>
     public class Noeud<T>
     {
         /// <summary>
-        /// Id unique du noeud.
+        /// Id du noeud.
         /// </summary>
         private int id;
 
         /// <summary>
-        /// Nom du nœud (ici, ça sera le nom d'une station).
+        /// Nom du noeud.
         /// </summary>
         private string nom;
 
@@ -35,16 +35,16 @@ namespace PROJET_PSI
         private float y;
 
         /// <summary>
-        /// Crée un nouveau nœud avec un identifiant donné.
+        /// Crée un nouveau noeud avec un identifiant donné.
         /// </summary>
-        /// <param name="id">Id unique du nœud.</param>
+        /// <param name="id">Id unique du noeud.</param>
         public Noeud(int id)
         {
             this.id = id;
         }
 
         /// <summary>
-        /// Obtient ou modifie l'id du nœud.
+        /// Obtient ou modifie l'ID du nœud.
         /// </summary>
         public int Id
         {
@@ -71,7 +71,7 @@ namespace PROJET_PSI
         }
 
         /// <summary>
-        /// Obtient ou modifie la coordonnée X du nœud (pour l'affichage).
+        /// Obtient ou modifie la coordonnée X du noeud (pour l'affichage).
         /// </summary>
         public float X
         {
@@ -80,7 +80,7 @@ namespace PROJET_PSI
         }
 
         /// <summary>
-        /// Obtient ou modifie la coordonnée Y du nœud (pour l'affichage).
+        /// Obtient ou modifie la coordonnée Y du noeud (pour l'affichage).
         /// </summary>
         public float Y
         {
@@ -89,4 +89,3 @@ namespace PROJET_PSI
         }
     }
 }
-
