410c9e18f3643381d54cdbf6fa924c390f7a05f2
Th1b00 <michelt0912@gmail.com>
2025-05-05 08:37:16 +0200

    version fiinale main

--- Projet_LivinParis/Program.cs
+++ Projet_LivinParis/Program.cs
@@ -1,79 +1,132 @@
-using PROJET_PSI;
 using System;
 using System.Linq;
 
 namespace PROJET_PSI
 {
-    /// <summary>
-    /// Classe principale de l'application, contenant le point d'entrée du programme.
-    /// Gère le menu principal et les appels aux modules de graphe et SQL.
-    /// </summary>
     public class Program
     {
         /// <summary>
-        /// Point d'entrée de l'application.
-        /// Affiche un menu pour accéder aux fonctionnalités du graphe du métro ou à l'application Livin Paris.
+        /// Affiche un menu permettant à l'utilisateur de :
+        /// 1 - Lancer l'application Livin'Paris
+        /// 2 - Analyser le graphe (coloration, bipartisme, planarité)
+        /// 3 - Exporter le graphe au format JSON ou XML
+        /// 4 - Tester les fonctionnalités d'import/export JSON et XML
+        /// 5 - Quitter l'application
         /// </summary>
-        /// <param name="args">Arguments.</param>
         static void Main(string[] args)
         {
+            var graphe = new Graphe<int>();
+            graphe.ChargerGraphe("metro.xlsx");
+
             bool quitter = false;
-            while (!quitter)
+
+            while (quitter == false)
             {
                 Console.Clear();
                 Console.WriteLine("=== Menu Principal ===");
-                Console.WriteLine("1. Requêtes graphe métro");
-                Console.WriteLine("2. Application Liv'in Paris (SQL)");
-                Console.WriteLine("3. Quitter");
+                Console.WriteLine("1. Lancer Livin'Paris");
+                Console.WriteLine("2. Analyser le graphe (Welsh-Powell, Biparti, Planarité)");
+                Console.WriteLine("3. Exporter le graphe (JSON/XML)");
+                Console.WriteLine("4. Tester Export/Import JSON et XML");
+                Console.WriteLine("5. Quitter");
                 Console.Write("Choix : ");
+
                 string choix = Console.ReadLine();
 
                 switch (choix)
                 {
                     case "1":
-                        var graphe = new Graphe<int>();
-                        graphe.ChargerGraphe("metro.xlsx");
-                        Console.WriteLine("\nOrdre du graphe : " + graphe.Noeuds.Count);
-
-                        int taille = 0;
-                        foreach (var noeud in graphe.Noeuds.Values)
-                        {
-                            taille += noeud.Liens.Count;
-                        }
-                        Console.WriteLine("Taille du graphe : " + ((taille / 2) - 1));
-                        Console.WriteLine("\nGénération du dessin du graphe complet...");
-                        graphe.AfficherGrapheGeo("graphe_metro_complet.png");
-                        Console.WriteLine("Le graphe complet a été enregistré sous 'graphe_metro_complet.png'.");
-
-                        Console.WriteLine("\nEntre quels sommets souhaitez-vous calculer le plus court chemin ?");
-                        int sommetA = graphe.LireSommetValide("Sommet de départ : ");
-                        int sommetB = graphe.LireSommetValide("Sommet d'arrivée : ");
-
-                        Console.WriteLine("\nDistance entre " + sommetA + " et " + sommetB + " :");
-                        Console.WriteLine("Dijkstra       : " + graphe.Dijkstra(sommetA, sommetB) + " minutes");
-                        Console.WriteLine("Bellman-Ford   : " + graphe.BellmanFord(sommetA, sommetB) + " minutes");
-                        Console.WriteLine("Floyd-Warshall : " + graphe.FloydWarshall(sommetA, sommetB) + " minutes");
-
-                        Console.WriteLine("\nGénération du graphe du chemin...");
-                        Console.WriteLine("Le graphe du chemin de " + sommetA + " à " + sommetB + " a été généré sous 'chemin_" + sommetA + "_vers_" + sommetB + ".png'");
-
-                        string cheminImage = "chemin_" + sommetA + "_vers_" + sommetB + ".png";
-                        graphe.AfficherCheminDansFenetre(cheminImage);
-
-                        Console.WriteLine("\nAppuyez sur une touche pour revenir au menu.");
-                        Console.ReadKey();
+                        MYSQL.AfficherMenuPrincipal(graphe);
                         break;
 
                     case "2":
-                        MYSQL.AfficherMenu();
+                        Console.Clear();
+                        graphe.AfficherStationsAvecColoration();
+
+                        int nbCouleurs = graphe.ColorationWelshPowell().Values.Distinct().Count();
+                        Console.WriteLine("Le graphe est " + nbCouleurs + "-Coloriable");
+
+                        if (graphe.EstBiparti() == true)
+                        {
+                            Console.WriteLine("Le graphe est biparti");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Le graphe n'est pas biparti");
+                        }
+
+                        if (graphe.EstPlanaire() == true)
+                        {
+                            Console.WriteLine("Le graphe est plannaire");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Le graphe n'est pas plannaire");
+                        }
+
+                        Console.WriteLine("\nAppuie sur une touche pour revenir au menu...");
+                        Console.ReadKey();
                         break;
 
                     case "3":
+                        Console.Clear();
+                        Console.WriteLine("=== Export du Graphe ===");
+                        Console.WriteLine("1. Exporter en JSON");
+                        Console.WriteLine("2. Exporter en XML");
+                        Console.Write("Choix : ");
+
+                        string exportChoix = Console.ReadLine();
+
+                        if (exportChoix == "1")
+                        {
+                            graphe.ExportJson("graphe.json");
+                            Console.WriteLine("Export JSON effectué (graphe.json).");
+                        }
+                        else if (exportChoix == "2")
+                        {
+                            graphe.ExportXml("graphe.xml");
+                            Console.WriteLine("Export XML effectué (graphe.xml).");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Choix invalide.");
+                        }
+
+                        Console.WriteLine("\nAppuie sur une touche pour revenir au menu...");
+                        Console.ReadKey();
+                        break;
+
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("=== Test Export/Import JSON/XML ===");
+
+                        string jsonPath = "test_export.json";
+                        string xmlPath = "test_export.xml";
+
+                        graphe.ExportJson(jsonPath);
+                        Console.WriteLine("Export JSON : " + jsonPath);
+
+                        graphe.ExportXml(xmlPath);
+                        Console.WriteLine("Export XML  : " + xmlPath);
+
+                        var grapheJson = new Graphe<int>();
+                        grapheJson.ImportJson(jsonPath);
+                        Console.WriteLine("Import JSON : " + grapheJson.Noeuds.Count + " sommets chargés");
+
+                        var grapheXml = new Graphe<int>();
+                        grapheXml.ImportXml(xmlPath);
+                        Console.WriteLine("Import XML  : " + grapheXml.Noeuds.Count + " sommets chargés");
+
+                        Console.WriteLine("\nAppuie sur une touche pour revenir au menu...");
+                        Console.ReadKey();
+                        break;
+
+                    case "5":
                         quitter = true;
                         break;
 
                     default:
-                        Console.WriteLine("Choix invalide. Appuyez sur une touche pour réessayer.");
+                        Console.WriteLine("Choix invalide.");
                         Console.ReadKey();
                         break;
                 }
